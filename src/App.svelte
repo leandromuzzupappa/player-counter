bb5a07efa5ac4fd703c0289a3295d241500d4adc
lmuzzupappa <lmuzzupappa@glamit.com.ar>
2022-01-14 17:54:34 -0300

    score counter

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import PlayerScore from "./PlayerScore.svelte";
+
+  let playerOneComponent;
+  let playerOneScore = 0;
+  let playerTwoComponent;
+  let playerTwoScore = 0;
+
+  // Probablemente necesite algun tipo de estado
+  function reset() {
+    playerOneScore = 0;
+    playerTwoScore = 0;
+  }
+</script>
+
+<main>
+  <h1 class="center">Player Score Counter</h1>
+  <PlayerScore bind:playerScore={playerOneScore} playerName="Lenny" />
+  <PlayerScore bind:playerScore={playerTwoScore} playerName="Lu" />
+  <button on:click={reset} class="contrast outline">RESET</button>
+</main>
---
+++ src/PlayerScore.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let playerName;
+  export let playerScore;
+
+  function increasePlayer1Score() {
+    playerScore += 1;
+  }
+  function decreasePlayer1Score() {
+    playerScore -= 1;
+    if (playerScore <= 0) {
+      playerScore = 0;
+    }
+  }
+</script>
+
+<div>
+  <h2 class="center">{playerName}</h2>
+  <h3 class="center">{playerScore}</h3>
+  <button on:click={increasePlayer1Score}>Up</button>
+  <button on:click={decreasePlayer1Score}>Down</button>
+</div>
+
+<style>
+  .center {
+    text-align: center;
+  }
+</style>
